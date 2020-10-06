761ad0a0fe7feec2440d54f00dbb3bcb2c11d567
Elzhas <esuleymenov@at-consulting.ru>
2020-10-07 01:46:22 +0600

    initial commit

---
+++ src/main/java/kz/parser/etml/Etml.java
@@ -0,0 +1,11 @@
+package kz.parser.etml;
+
+public final class Etml {
+
+    public <T> T fromHtml(String html, String configuration, Class<T> classOf) {
+        T target = null;
+
+        return target;
+    }
+
+}
---
+++ src/test/java/kz/parser/etml/TestEtml.java
@@ -0,0 +1,12 @@
+package kz.parser.etml;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestEtml {
+
+    @Test
+    public void testGeneration() {
+        Etml etml = new Etml();
+    }
+}
